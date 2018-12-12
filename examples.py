d30575c8e79c32d465b7d2362ff2b516de83c2e3
HamzixX <hamza_ultras@live.fr>
2018-12-12 16:31:22 +0100

    third Commit

--- examples.py
+++ examples.py
@@ -1,9 +1,12 @@
-a =int(input("gine a number \n"))
-s=0
-for i in range (1,a) :
-    if (a % 2) == 0 :
-        s=s+1
-if s > 2 :
-    print("not a prime number")
-else:
-    print("prime number")
+def prime():
+ a =int(input("gine a number \n"))
+ s=0
+ for i in range (1,a) :
+     if (a % 2) == 0 :
+         s=s+1
+ if s > 2 :
+     print("not a prime number")
+ else:
+     print("prime number")
+prime()
+
