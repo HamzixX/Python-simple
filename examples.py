099ab883eee1320b7949cbbd446c8978c504f09c
HamzixX <hamza_ultras@live.fr>
2018-12-12 16:49:18 +0100

    third Commit

--- examples.py
+++ examples.py
@@ -1,12 +1,18 @@
 def prime():
- a =int(input("gine a number \n"))
  s=0
+ a =int(input("give a number \n"))
  for i in range (1,a) :
      if (a % 2) == 0 :
          s=s+1
  if s > 2 :
-     print("not a prime number")
+     print("not a prime number \n")
  else:
-     print("prime number")
+     print("prime number \n")
+def power() :
+    p=0
+    a=int(input("give a number \n"))
+    x=int(input("give power \n"))
+    print("the result is",a**x)
 prime()
+power()
 
