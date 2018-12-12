7ed5de5f09c18f63c648b807ff9c724494329b2a
HamzixX <hamza_ultras@live.fr>
2018-12-12 16:28:56 +0100

    Second Commit

--- examples.py
+++ examples.py
@@ -1,2 +1,9 @@
-a=2
-print(a)
+a =int(input("gine a number \n"))
+s=0
+for i in range (1,a) :
+    if (a % 2) == 0 :
+        s=s+1
+if s > 2 :
+    print("not a prime number")
+else:
+    print("prime number")
